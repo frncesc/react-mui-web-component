dd28018e2f856e84f3f84057fa82e00f16d103fc
Francesc Busquets <francesc@gmail.com>
2025-12-30 20:45:03 +0100

    Add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
+/* global process */
 
 import path from 'node:path';
 import fs from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import pkg from './package.json' with {type: 'json'};
+import dotenv from 'dotenv';
 import Dotenv from 'dotenv-webpack';
 import TerserPlugin from 'terser-webpack-plugin';
 import webpack from 'webpack';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const date = new Date();
 const version = `${pkg.version} (${date.toISOString().substring(0, 10)})`;
@@ -41,7 +42,8 @@ SOFTWARE.
 For full license information of included components please see: 
 components.LICENSE
 
-WARNING: This is a compressed version of "${pkg.name}". Full source code is freely available at:
+WARNING: This is a compressed version of "${pkg.name}".
+Full source code is freely available at:
 ${pkg.repository.url}
 `;
 
@@ -64,90 +66,99 @@ const assetRules = [
   },
 ];
 
-/**
- * Main config
- */
-const config = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: `${pkg.name}.min.js`,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-            ],
-          }
-        },
-      },
-      ...assetRules,
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      APP_ID: JSON.stringify(`${pkg.title} v${version}`),
-    }),
-  ],
-  devServer: {
-    host: 'localhost',
-    port: 8000,
-    open: true,
-    static: {
-      directory: path.join(__dirname, 'public'),
-      watch: true,
-    },
-    client: {
-      overlay: true,
-      progress: true,
-    },
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: {
-          filename: `${pkg.name}.components.LICENSE`,
-          banner: () => banner,
-        },
-        terserOptions: {
-          // See: https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-        }
-      }),
-    ],
-  },
-  performance: {
-    // Avoid "big asset" warnings
-    maxAssetSize: 2000000,
-    maxEntrypointSize: 2000000,
-  },
-};
-
 export default function (env, argv) {
 
-  const prod = argv.mode === 'production';
+  const MODE = argv.mode || '';
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  let dotEnvPath = path.resolve(__dirname, `.env${MODE ? '.' : ''}${MODE}`);
+  if (!fs.existsSync(dotEnvPath))
+    dotEnvPath = path.resolve(__dirname, '.env');
+  if (!fs.existsSync(dotEnvPath))
+    dotEnvPath = path.resolve(__dirname, '.env.example');
+  dotenv.config({ path: dotEnvPath });
 
-  // Take environment variables from ".env.development", ".env.production"
-  // or just ".env" when `mode` is not set
-  config.plugins = [
-    ...config.plugins,
-    new Dotenv({
-      path: (argv.mode && fs.existsSync(path.resolve(__dirname, `.env.${argv.mode}`))) ? `./.env.${argv.mode}` : './.env',
-      safe: './.env.example',
-      allowEmptyValues: true,
-    })
-  ];
-
-  // Create source maps only in production builds
-  if (prod)
-    config.devtool = 'source-map';
+  const config = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: `${process.env.COMPONENT_NAME}.js`,
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+              ],
+            }
+          },
+        },
+        ...assetRules,
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        APP_ID: JSON.stringify(`${pkg.title} v${version}`),
+      }),
+      new Dotenv({
+        path: dotEnvPath,
+        safe: './.env.example',
+        allowEmptyValues: true,
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'public/index.html',
+        minify: false,
+        scriptLoading: 'module',
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'index-nowc.html',
+        template: 'public/index-nowc.html',
+        minify: false,
+        scriptLoading: 'module',
+      }),
+    ],
+    devServer: {
+      host: 'localhost',
+      port: 8000,
+      open: true,
+      static: {
+        directory: path.join(__dirname, 'public'),
+        watch: true,
+      },
+      client: {
+        overlay: true,
+        progress: true,
+      },
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: {
+            filename: `${process.env.COMPONENT_NAME}.LICENSES`,
+            banner: () => banner,
+          },
+          terserOptions: {
+            // See: https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+          }
+        }),
+      ],
+    },
+    performance: {
+      // Avoid "big asset" warnings
+      maxAssetSize: 2000000,
+      maxEntrypointSize: 2000000,
+    },
+    // Create source maps only in production builds
+    devtool: MODE === 'production' ? 'source-map' : undefined,
+  };
 
   return config;
 };
